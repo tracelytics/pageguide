afba55f1b81bbd2126ba32254b919411b97e96fd
Ari Rizzitano <arizzitano@gmail.com>
2013-12-27 12:48:37 -0500

    [testsv1] add fn for testing dom operations on the pg example page.

--- js/tests/unit/test_pageguide.js
+++ js/tests/unit/test_pageguide.js
@@ -1,13 +1,29 @@
 $(function () {
 
-    module("pageguide")
+    module("tl.pg");
 
-      test("hello world", function () {
-        ok(true);
-      })
-
-      test("pageguide exists", function () {
+    test("pageguide exists", function () {
         ok(tl.pg);
-      })
+    });
 
+    module("DOM operations");
+
+    loadFixtureAndTest('fixture loaded', function () {
+        ok($('#exampleContent').length);
+    });
+
+    /**
+     * for testing pageguide actions in the DOM: load example page HTML into the
+     * qunit fixture element, then run the callback.
+     * - title: title of unit being tested
+     * - cb: callback function containing the body of the test.
+     **/
+    function loadFixtureAndTest (title, cb) {
+        asyncTest(title, function () {
+            $('#qunit-fixture').load('../../example/index.html #exampleContent', function () {
+                cb();
+                start();
+            });
+        });
+    }
 })
