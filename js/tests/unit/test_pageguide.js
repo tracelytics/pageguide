c6d462a57bd859713f52553f63d33f13b0ef7e4e
Ari Rizzitano <arizzitano@gmail.com>
2013-12-30 16:55:54 -0500

    comments

--- js/tests/unit/test_pageguide.js
+++ js/tests/unit/test_pageguide.js
@@ -7,12 +7,16 @@ $(function () {
     });
 
     /**
-     * for testing pageguide actions in the DOM: load example page HTML into the
+     * for testing pageguide load in the DOM: load example page HTML into the
      * qunit fixture element, then run the callback.
      * - title: title of unit being tested
      * - cb: callback function containing the body of the test.
+     * - delayStart: optional boolean indicating whether to defer start() (meaning
+     *   it will be handled within the callback)
+     * - selector: optional string selector for the area to select within the
+     *   example page
      **/
-    function loadFixtureAndTest (title, cb, delayStart, selector) {
+    function loadAndTest (title, cb, delayStart, selector) {
         var sel = selector || '#exampleContent';
         asyncTest(title, function () {
             $('#qunit-fixture').load('../../example/index.html '+sel, function () {
@@ -25,6 +29,26 @@ $(function () {
         });
     }
 
+    /**
+     * for running a test on pageguide ready. waits until pg.ready() fires to
+     * run the test. useful for integration-type things (user interactions, etc)
+     * - title: title of unit being tested
+     * - cb: callback function containing the body of the test
+     * - selector: optional string selector for the area to select within the
+     *   example page
+     **/
+    function loadInitAndTest (title, cb, selector) {
+        loadAndTest(title, function () {
+            tl.pg.init({
+                ready_callback: function () {
+                    cb();
+                    start();
+                    cleanup();
+                }
+            });
+        }, true, selector);
+    }
+
     function cleanup () {
         $('#tlyPageGuideWrapper').remove();
         $('#tlyPageGuideOverlay').remove();
@@ -33,13 +57,13 @@ $(function () {
 
     module("DOM: loading pageguide");
 
-    loadFixtureAndTest('fixture loaded', function () {
+    loadAndTest('fixture loaded', function () {
         expect(1);
 
         ok($('#exampleContent').length, 'fixture loaded correctly');
     });
 
-    loadFixtureAndTest('default elements exist', function () {
+    loadAndTest('default elements exist', function () {
         expect(2);
 
         tl.pg.init();
@@ -47,16 +71,22 @@ $(function () {
         ok($('.tlypageguide_toggle').length, 'toggle exists');
     });
 
-    loadFixtureAndTest('welcome exists', function () {
+    loadInitAndTest('welcome exists', function () {
         expect(3);
-        var pg = tl.pg.init({
-            ready_callback: function () {
-                ok($('#tlyPageGuideWrapper #tlyPageGuideWelcome').length, 'welcome inside wrapper');
-                equal($('#tlyPageGuideOverlay').length, 1, 'only one overlay exists');
-                ok($('body').hasClass('tlyPageGuideWelcomeOpen'));
-                start();
-                cleanup();
-            }
-        });
-    }, true, '#examplePlusWelcome');
+
+        ok($('#tlyPageGuideWrapper #tlyPageGuideWelcome').length, 'welcome inside wrapper');
+        equal($('#tlyPageGuideOverlay').length, 1, 'only one overlay exists');
+        ok($('body').hasClass('tlyPageGuideWelcomeOpen'), 'body class');
+    }, '#examplePlusWelcome');
+
+
+    module("DOM: basic interaction");
+
+    loadInitAndTest('open', function () {
+        expect(2);
+
+        $('.tlypageguide_toggle').trigger('click');
+        ok($('body').hasClass('tlypageguide-open'), 'body class');
+        ok($('#tlyPageGuideMessages').is(':visible'), 'message area shown');
+    });
 })
