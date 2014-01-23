b30a5bb709ac7d4ff8ad60e2a7bc17c8318f798d
Ari Rizzitano <arizzitano@gmail.com>
2014-01-23 15:52:39 -0500

    [benchmark] add crude results display method

--- js/tests/benchmark/test_speed_pageguide.js
+++ js/tests/benchmark/test_speed_pageguide.js
@@ -1,3 +1,24 @@
+Benchmark.Suite.prototype.setupDisplay = function () {
+    var self = this;
+    var $results = $('#benchmarkResults');
+    console.log('asdf');
+    _.each(self, function (benchmark, i) {
+        $results.append(
+            '<tr id="result-' + (i + 1) + '">' +
+                '<td class="summary">ready</td>' +
+            '</tr>'
+        );
+    });
+
+    self.on('cycle', function (event) {
+        var $row = $results.find('#result-' + event.target.id);
+        $row.find('.summary').text(String(event.target));
+    })
+    .on('complete', function () {
+        console.log('complete');
+    });
+};
+
 $(function () {
 $(document).ready(function () {
 $('#test_element').load('../../../example/index.html #exampleContent', function () {
@@ -6,16 +27,14 @@ $('#test_element').load('../../../example/index.html #exampleContent', function 
     pgSuite.add('open', function () {
         ['asdf','jhfghdfgh',45,'ghfgh','8905830945830','sdfsdfgsdfg',null,6].indexOf(6);
     })
-    .on('cycle', function (event) {
-        console.log(String(event.target));
-    })
-    .on('complete', function () {
-        console.log('complete');
+    .add('opensdfgsdf', function () {
+        ['asdf','jhfghdfgh',45,'ghfgh','8905830945830','sdfsdfgsdfg',null,6].indexOf(6);
     });
 
-    console.log(suite);
-});
-});
+    pgSuite.setupDisplay();
+    pgSuite.run();
 
+});
+});
 }(jQuery));
 
