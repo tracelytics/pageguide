a7f143a06f4c7120623a7fde477182629d7d0ffa
Ari Rizzitano <arizzitano@gmail.com>
2014-01-23 15:32:48 -0500

    [benchmark] use benchmark.js instead of jslitmus

--- js/tests/benchmark/test_speed_pageguide.js
+++ js/tests/benchmark/test_speed_pageguide.js
@@ -1,6 +1,21 @@
 $(function () {
+$(document).ready(function () {
+$('#test_element').load('../../../example/index.html #exampleContent', function () {
+    var pgSuite = new Benchmark.Suite('Pageguide Speed Suite');
 
-    JSLitmus.test('empty test (non-looping)', function() {});
+    pgSuite.add('open', function () {
+        ['asdf','jhfghdfgh',45,'ghfgh','8905830945830','sdfsdfgsdfg',null,6].indexOf(6);
+    })
+    .on('cycle', function (event) {
+        console.log(String(event.target));
+    })
+    .on('complete', function () {
+        console.log('complete');
+    });
+
+    console.log(suite);
+});
+});
 
 }(jQuery));
 
