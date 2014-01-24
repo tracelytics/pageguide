23943713b9474ea03ab4235bb8788e90b8bde6ab
Ari Rizzitano <arizzitano@gmail.com>
2014-01-24 12:46:19 -0500

    [benchmark] test open, _on_expand, and init

--- js/tests/benchmark/test_speed_pageguide.js
+++ js/tests/benchmark/test_speed_pageguide.js
@@ -1,7 +1,50 @@
+var pg = {};
+
+$(function () {
+$(document).ready(function () {
+$('#test_element').load('../../../example/index.html #exampleContent', function () {
+    var pgSuite = new Benchmark.Suite('Pageguide Speed Suite');
+    var benchProto = {
+        onError: function (e) {
+            console.log(e);
+        }
+    }
+
+    pg = tl.pg.init({
+        ready_callback: function () {
+            pgSuite.run();
+        }
+    });
+
+    pgSuite.add('_open', _.extend(benchProto, {
+        fn: function () {
+            pg._open();
+        }
+    }))
+    .add('_on_expand', _.extend(benchProto, {
+        fn: function () {
+            pg._on_expand();
+        }
+    }))
+    .add('init', _.extend(benchProto, {
+        defer: true,
+        fn: function (deferred) {
+            pg = tl.pg.init({
+                ready_callback: function () {
+                    deferred.resolve();
+                }
+            });
+        }
+    }));
+
+    pgSuite.setupDisplay();
+});
+});
+}(jQuery));
+
 Benchmark.Suite.prototype.setupDisplay = function () {
     var self = this;
     var $results = $('#benchmarkResults');
-    console.log('asdf');
     _.each(self, function (benchmark, i) {
         $results.append(
             '<tr id="result-' + (i + 1) + '">' +
@@ -13,28 +56,10 @@ Benchmark.Suite.prototype.setupDisplay = function () {
     self.on('cycle', function (event) {
         var $row = $results.find('#result-' + event.target.id);
         $row.find('.summary').text(String(event.target));
+        console.log(String(event.target));
     })
     .on('complete', function () {
         console.log('complete');
     });
 };
-
-$(function () {
-$(document).ready(function () {
-$('#test_element').load('../../../example/index.html #exampleContent', function () {
-    var pgSuite = new Benchmark.Suite('Pageguide Speed Suite');
-
-    pgSuite.add('open', function () {
-        ['asdf','jhfghdfgh',45,'ghfgh','8905830945830','sdfsdfgsdfg',null,6].indexOf(6);
-    })
-    .add('opensdfgsdf', function () {
-        ['asdf','jhfghdfgh',45,'ghfgh','8905830945830','sdfsdfgsdfg',null,6].indexOf(6);
-    });
-
-    pgSuite.setupDisplay();
-    pgSuite.run();
-
-});
-});
-}(jQuery));
 
